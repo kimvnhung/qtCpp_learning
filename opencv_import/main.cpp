8307bef66825453eeeb7b7e92615a596654124e9
kimvnhung <kimvnhung@gmail.com>
2024-06-04 16:23:56 +0700

    imshow success

--- opencv_import/main.cpp
+++ opencv_import/main.cpp
@@ -1,4 +1,5 @@
 #include <QCoreApplication>
+#include <QFile>
 
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
@@ -11,14 +12,24 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    // Show image from images.qrc using imshow
-    // Load image from resources
-    Mat image = imread(":/images/crop1.bmp");
+    // Check if qrc file path is correct
+    QFile file(":/images/crop1.bmp");
+    if (!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Error opening file";
+        return -1;
+    }
+
+    QByteArray imageData = file.readAll();
+    cv::Mat buf(1, imageData.size(), CV_8UC1, imageData.data());
+    cv::Mat image = cv::imdecode(buf, cv::IMREAD_COLOR);
+
     if (image.empty()) {
         qDebug() << "Error loading image";
         return -1;
     }
+
     imshow("windowName", image);
+
     waitKey(0);
     return a.exec();
 }
